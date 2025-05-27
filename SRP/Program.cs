1da0563336abac7740130a778c596e50d6cc7c90
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-27 12:06:12 +0600

    Sigle responsible principle

--- SRP/Program.cs
+++ SRP/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace SRP
+{
+    public class MemberShip   // Class should solve only one responsible not multiple
+    {
+        public void Add()
+        {
+            try
+            {
+                // TO DO
+            }
+            catch (Exception ex)
+            {
+                FIleLogger.LogError(ex.Message);
+            }
+
+        }
+    }
+    public static class FIleLogger
+    {
+        public static void LogError(string message)
+        {
+             File.WriteAllText("An error occurred while adding a member.", message);
+        }
+    }
+
+
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+         
+         
+    }
+
+
+
+}
